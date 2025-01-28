23592a25a8f39e8d6c1d981e6059f9cd9fd3c006
Francisiek <franciszek.koral@gmail.com>
2025-01-28 02:31:38 +0100

    scripts

---
+++ install_all.bat
@@ -0,0 +1,8 @@
+curl.exe https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe
+start /w "" "Docker Desktop Installer.exe" install
+net localgroup docker-users <user> /add
+
+curl.exe https://github.com/Francisiek/pnk2025/archive/refs/heads/main.zip
+tar.exe -xf main.zip
+cd pnk2025-main
+docker compose up --build
