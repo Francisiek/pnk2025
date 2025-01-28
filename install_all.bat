c8081e031ed6978e63dad0aa77b51fa14a1c5a27
Francisiek <franciszek.koral@gmail.com>
2025-01-28 18:41:38 +0100

    install

--- install_all.bat
+++ install_all.bat
@@ -1,4 +1,4 @@
-curl.exe https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe
+curl.exe https://desktop.docker.com/win/main/amd64/Docker%20Desktop%20Installer.exe?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-win-amd64
 start /w "" "Docker Desktop Installer.exe" install
 net localgroup docker-users <user> /add
 
